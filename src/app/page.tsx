129f69266e9783cdcc1d286d536fd39fde2cae96
qpwoei0123 <qpwoei0123@naver.com>
2023-11-01 14:49:55 +0900

    Feat: 에러&로딩 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 import Link from "next/link";
 import style from "./style.module.css";
 
-export default function Home() {
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+const getCurrentWeather = async () => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=seoul&aqi=no`
+  );
+  if (!res.ok) {
+    if (!res.ok) throw new Error(`날씨정보 못 가져옴`);
+  }
+  return res.json();
+};
+
+export default async function Home() {
+  const res = await getCurrentWeather();
+  console.log(res);
+
   return (
     <>
       <h1>날씨 앱</h1>
