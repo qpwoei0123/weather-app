ed293a9d62c29ad9cfbf01b4813e86fe3a53a48f
qpwoei0123 <qpwoei0123@naver.com>
2023-11-08 18:20:18 +0900

    Feat: 재검증

---
+++ src/component/RevalidateButton.tsx
@@ -0,0 +1,13 @@
+import revalidate from "@/app/utils/revalidate";
+
+type Props = {
+  tag: string;
+};
+
+export default function RevalidateButton({ tag }: Props) {
+  const handleClick = async () => {
+    await revalidate(tag);
+  };
+
+  return <button onClick={handleClick}>캐시 비우기</button>;
+}
