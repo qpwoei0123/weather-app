bbbf8384c905a6e5a2cc2ec6d42162eee37f64db
qpwoei0123 <qpwoei0123@naver.com>
2023-11-08 18:30:58 +0900

    Feat: 재검증 버튼 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import style from "./style.module.css";
 import { getCurrentWeather } from "./utils/getCrurrentWeather";
+import { getTime } from "./utils/getTime";
+import RevalidateButton from "@/component/RevalidateButton";
 
 export default async function Home() {
   const res = await getCurrentWeather("seoul");
-  console.log(res.current.condition.text);
-  const locations = ["seoul", "newyork", "rondon"];
-
+  const time = await getTime(res.location.tz_id);
   return (
     <>
       <h1>날씨 앱</h1>
+      {time.dateTime}
       <ul className={style.list}>
         <li>
           <Link href="seoul">서울</Link>
@@ -22,6 +23,7 @@ export default async function Home() {
           <Link href="rondon">런던</Link>
         </li>
       </ul>
+      <RevalidateButton tag={"time"} />
     </>
   );
 }
--- src/component/RevalidateButton.tsx
+++ src/component/RevalidateButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import revalidate from "@/app/utils/revalidate";
 
 type Props = {
@@ -7,6 +8,7 @@ type Props = {
 export default function RevalidateButton({ tag }: Props) {
   const handleClick = async () => {
     await revalidate(tag);
+    window.location.reload();
   };
 
   return <button onClick={handleClick}>캐시 비우기</button>;
