50e0a94184f04f84a91953e1c5bfe906927dea87
qpwoei0123 <qpwoei0123@naver.com>
2023-11-30 22:05:48 +0900

    코드기반 이미지반환 함수

---
+++ src/app/utils/getWeatherImg.ts
@@ -0,0 +1,33 @@
+import { StaticImageData } from "next/image";
+import clear from "../../asset/clear.jpg";
+import cloudy from "../../asset/cloudy.png";
+import rain from "../../asset/rain.png";
+import snow from "../../asset/snow.png";
+
+export const getWeatherImg = async (code: number): Promise<StaticImageData> => {
+  const weatherCodes = {
+    clear: [1000],
+    cloudy: [1003, 1006, 1009, 1030, 1072, 1135, 1147],
+    rain: [
+      1063, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198,
+      1201, 1240, 1243, 1246, 1273, 1276,
+    ],
+  };
+  const isClear = weatherCodes.clear.includes(code);
+  const isCloudy = weatherCodes.cloudy.includes(code);
+  const isRainy = weatherCodes.rain.includes(code);
+
+  if (isClear) {
+    console.log("Clear weather");
+    return clear;
+  } else if (isCloudy) {
+    console.log("Cloudy weather");
+    return cloudy;
+  } else if (isRainy) {
+    console.log("rain weather");
+    return rain;
+  } else {
+    console.log("snow weather");
+    return snow;
+  }
+};
