91810016038b16facf6a26ddbd5a55bd9677577b
qpwoei0123 <qpwoei0123@naver.com>
2023-12-05 02:12:51 +0900

    내위치의 날씨 페이지

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -1,37 +1,74 @@
 import HomeButton from "@/component/HomeButton";
 import { getForecast } from "../utils/getForecast";
-import RevalidateButton from "@/component/RevalidateButton";
+import style from "./style.module.css";
+import Image from "next/image";
+import { getTime } from "../utils/getTime";
+import { getWeatherImg } from "../utils/getWeatherImg";
 
 type Props = {
   params: {
     location: string;
   };
   searchParams: {
-    name: string;
+    lon: number;
+    lat: number;
   };
 };
 
-export function generateMetadata({ searchParams }: Props) {
+// 메타 데이터 자동생성
+export function generateMetadata({ params }: Props) {
+  const location = decodeURIComponent(params.location);
   return {
-    title: `날씨앱 - ${searchParams.name}`,
-    discription: `${searchParams.name} 의날씨를 알려드림`,
+    title: `날씨앱 - ${location}`,
+    discription: `${location} 의날씨를 알려드림`,
   };
 }
 
-export default async function Detail({ params, searchParams }: Props) {
-  const name = searchParams.name;
-  const res = await getForecast(params.location);
+export default async function Location({ params, searchParams }: Props) {
+  const name = decodeURIComponent(params.location);
+  const forecast = await getForecast(searchParams.lat, searchParams.lon);
+  const time = await getTime(forecast.location.tz_id);
+  const weatherSrc = await getWeatherImg(forecast.current.condition.code);
 
   return (
     <>
-      <h1> {name} 의 일기예보 </h1>
-      {res.forecast.forecastday.map((el) => (
-        <li key={el.date}>
-          {el.date} / {el.day.avgtemp_c}
-        </li>
-      ))}
-      <HomeButton />
-      <RevalidateButton tag={"forecast"} />
+      <Image
+        src={weatherSrc}
+        alt="backgroundImg"
+        className={style.backgroundImg}
+      />
+      <div className={style.container}>
+        <div className={style.mainDiv}>
+          <div className={style.firstBox}>
+            <p>{name}</p>
+            <p>{forecast.current.temp_c}℃</p>
+          </div>
+          <div className={style.secondBox}>
+            <p>{forecast.current.condition.text} </p>
+          </div>
+          <div className={style.thirdBox}>
+            <p> {time.dayOfWeek}</p>
+            <p> {time.date}</p>
+          </div>
+        </div>
+        <div className={style.sideBar}>
+          <div className={style.topBox}>
+            <HomeButton />
+          </div>
+          <div className={style.bottomBox}>
+            <h1>ForeCast</h1>
+            {forecast.forecast.forecastday.map((el) => (
+              <div key={el.date} className={style.day}>
+                <ul>
+                  {el.date.slice(5)}
+                  <li>{el.day.avgtemp_c}℃</li>
+                  <li>{el.day.condition.text}</li>
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
