933f6fde71bea91e363616db07098ff973ce5795
qpwoei0123 <qpwoei0123@naver.com>
2023-12-05 02:04:44 +0900

    위치엑세스 권한 분기 추가

--- src/component/MyCityDiv.tsx
+++ src/component/MyCityDiv.tsx
@@ -1,28 +1,57 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { getCityInfo } from "@/app/utils/getCityInfo";
+import { CityInfo, getCityInfo } from "@/app/utils/getCityInfo";
 import style from "./style.module.css";
+import Link from "next/link";
 
 export default function MyCityDiv() {
-  const [cityInfo, setCityInfo] = useState<string | null>(null);
+  const [cityInfo, setCityInfo] = useState<CityInfo | null>(null);
+  const [isGeolocation, setGeolocation] = useState<boolean>(false);
 
   useEffect(() => {
+    // 위치엑세스 권한 체크
+    const checkgeolocation = async () => {
+      const permissionStatus = await navigator.permissions.query({
+        name: "geolocation",
+      });
+      if (permissionStatus.state === "granted") {
+        setGeolocation(true);
+      } else {
+        setGeolocation(false);
+      }
+    };
+
+    // 현재 위치한 도시 데이터 가져오기
     const fetchData = async () => {
       try {
         const data = await getCityInfo();
-        setCityInfo(data?.cityName ?? null);
+        setCityInfo(data ? data : null);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchData();
-  }, []); // 빈 배열은 컴포넌트가 처음 마운트될 때만 실행됨
+    checkgeolocation();
+  }, []);
 
-  // cityInfo가 아직 없는 경우를 처리
+  // 위치엑세스 권한 유무, 도시정보 유무
   return (
-    <p className={style.myCity}>
-      {cityInfo ? `${cityInfo}의 날씨 알아보기.` : "Loading..."}
-    </p>
+    <>
+      {isGeolocation ? (
+        cityInfo ? (
+          <Link
+            href={`${cityInfo.cityName}?lon=${cityInfo.longitude}&lat=${cityInfo.latitude}`}
+            className={style.link}
+          >
+            {cityInfo.cityName}의 날씨는?
+          </Link>
+        ) : (
+          <p>위치를 불러오는 중...</p>
+        )
+      ) : (
+        <p>위치 액세스를 허용후 새로고침 해주세요!</p>
+      )}
+    </>
   );
 }
