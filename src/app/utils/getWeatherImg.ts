71126d6fb05efb62a89bad97d1f4abb1843102b2
qpwoei0123 <qpwoei0123@naver.com>
2023-12-06 16:36:10 +0900

    날씨코드 재분류

--- src/app/utils/getWeatherImg.ts
+++ src/app/utils/getWeatherImg.ts
@@ -8,8 +8,8 @@ import snow from "../../asset/snow.png";
 export const getWeatherImg = async (code: number): Promise<StaticImageData> => {
   // 응답 데이터 표를 보며 골라낸? 코드분류
   const weatherCodes = {
-    clear: [1000],
-    cloudy: [1003, 1006, 1009, 1030, 1072, 1135, 1147],
+    clear: [1000, 1003],
+    cloudy: [1006, 1009, 1030, 1072, 1135, 1147],
     rain: [
       1063, 1150, 1153, 1168, 1171, 1180, 1183, 1186, 1189, 1192, 1195, 1198,
       1201, 1240, 1243, 1246, 1273, 1276,
