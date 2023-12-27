0f76d2ac9d455a486be51792c9db09476d34983f
qpwoei0123 <qpwoei0123@naver.com>
2023-12-27 11:15:02 +0900

    로딩페이지 개선

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,8 +3,8 @@ import style from "./style.module.css";
 
 export default function Loading() {
   return (
-    <div className={style.loadingContainer}>
-      <div className={style.cloud}>기상정보를 가져오는중...</div>
+    <div className={`${style.loadingContainer} `}>
+      <div className={style.loader}></div>
     </div>
   );
 }
