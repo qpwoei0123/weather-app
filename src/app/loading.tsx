995c97159abcf1fe3f0d8c67be96133aaaf1a048
qpwoei0123 <qpwoei0123@naver.com>
2023-12-28 12:05:48 +0900

    로딩페이지 개선

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="flexCenter h-screen ">
-      <div className="animate-spin rounded-full border-t-8 border-blue-500 border-opacity-25 h-40 w-40 mb-4"></div>
+      <div className="animate-spin rounded-full border-t-8 border-blue-500  border-opacity-25 h-40 w-40 mb-4"></div>
     </div>
   );
 }
