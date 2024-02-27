a42f73288a63ec452222ccd2d4140945891cbd10
qpwoei0123 <qpwoei0123@naver.com>
2024-02-28 00:21:03 +0900

    try catch문 추가

--- src/app/utils/getCityName.ts
+++ src/app/utils/getCityName.ts
@@ -1,18 +1,27 @@
-type coords = {
+type Coords = {
   latitude: number;
   longitude: number;
 };
-export const getCityName = async ({ coords }: { coords: coords }) => {
-  const GOOGLEMAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLEMAPS_API_KEY;
-  const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.latitude},${coords.longitude}&key=${GOOGLEMAPS_API_KEY}`;
-  const res = await fetch(geocodingUrl);
-  const data = await res.json();
-  console.log(data);
-  const city = data.results?.[0]?.address_components?.find(
-    (component: any) =>
-      component.types.includes("sublocality") ||
-      component.types.includes("locality") ||
-      component.types.includes("political"),
-  );
-  return city ? city.long_name : "알수없는 어딘가...";
+
+export const getCityName = async ({
+  coords,
+}: {
+  coords: Coords;
+}): Promise<string> => {
+  try {
+    const GOOGLEMAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLEMAPS_API_KEY;
+    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.latitude},${coords.longitude}&key=${GOOGLEMAPS_API_KEY}`;
+    const res = await fetch(geocodingUrl);
+    const data = await res.json();
+    const city = data.results?.[0]?.address_components?.find(
+      (component: any) =>
+        component.types.includes("sublocality") ||
+        component.types.includes("locality") ||
+        component.types.includes("political"),
+    );
+    return city.long_name || "알 수 없는 어떤곳...";
+  } catch (error) {
+    console.error("지도 정보를 가져오는 도중 오류가 발생했습니다:", error);
+    return "알 수 없는 어떤곳...";
+  }
 };
