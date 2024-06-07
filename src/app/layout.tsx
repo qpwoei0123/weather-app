9c4a0bb0515734fcba15fdb4f35dbabeab9cea4c
qpwoei0123 <qpwoei0123@naver.com>
2024-06-07 18:31:06 +0900

    chore: import remixicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "remixicon/fonts/remixicon.css";
 import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
