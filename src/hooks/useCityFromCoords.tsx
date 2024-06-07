c32614655c76eb776dec681914724a65bb9d9101
qpwoei0123 <qpwoei0123@naver.com>
2024-06-07 18:43:38 +0900

    feat: 도시명 리턴 커스텀훅 구현

---
+++ src/hooks/useCityFromCoords.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useState } from "react";
+import { getCityName } from "@/app/utils/getCityName";
+
+type coords = {
+  latitude: number;
+  longitude: number;
+};
+
+export const useCityFromCoords = (coords: coords | undefined) => {
+  const [city, setCity] = useState<string | null>(null);
+  if (coords) {
+    getCityName({ coords: coords })
+      .then((city) => setCity(city))
+      .catch((error) =>
+        console.error("도시명을 가져오는 동안 에러 발생:", error),
+      );
+  }
+  return city;
+};
